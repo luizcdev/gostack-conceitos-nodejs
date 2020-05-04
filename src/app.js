dc6f362d755d691352365c7d8f9ea2936b28f164
Luiz Oliveira <luizcdev@gmail.com>
2020-05-03 23:19:35 -0300

    Desenvolvimento do projeto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,62 @@ app.use(cors());
 
 const repositories = [];
 
+function validateParamId( request, response, next)  {
+  const { id } = request.params;
+
+  const index = repositories.findIndex(repositorie => repositorie.id == id );
+  if ( index < 0 ) 
+    return response.status(400).json({ error: "Repositorie not found"});
+
+  request.params.index = index;
+
+  return next();
+
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  const repositorie = {id: uuid(), title, url, techs, likes: 0};
+
+  repositories.push(repositorie);
+
+  return response.json(repositorie);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateParamId, (request, response) => {
+  const { id, index } = request.params;  
+
+
+  const { title, techs, url } = request.body;
+  const { likes } = repositories[index];
+  repositories[index] = {
+    id,
+    title,
+    techs,
+    url,
+    likes
+  }
+
+  return response.json( repositories[index] );
+
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateParamId,  (request, response) => {
+  const { index } = request.params;
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateParamId, (request, response) => {
+  const { index } = request.params;
+  repositories[index].likes += 1;
+
+  return response.json( repositories[index]);
 });
 
 module.exports = app;
